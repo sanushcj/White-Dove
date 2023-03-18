6d01d495cf6cf2a69083238e98930ee377e6e6d1
Sanush CJ <sanushcj@gmail.com>
2023-03-19 01:18:23 +0530

    twitter theme added

---
+++ lib/theme/application_theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:whitedove/theme/pallete.dart';
+
+class TwitterTheme {
+  static ThemeData theme = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: Pallete.backgroundColor,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Pallete.backgroundColor,
+      elevation: 0,
+    ),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: Pallete.blueColor,
+    ),
+  );
+}
