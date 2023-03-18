7875fe650eb797206f0b330e86217bd66388e9b6
Sanush CJ <sanushcj@gmail.com>
2023-03-19 01:12:35 +0530

    added svgs and some constants

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppWriteConstants {
+  static const String databaseId = '6416108ba684cb31f31f';
+  static const String projectId = '6414e9fedb72000a0a84';
+  static const String endpoint = 'http://localhost:80/v1';
+}
